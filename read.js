02e0d9b3350f1aaba4175e5a449a46dd37c927e8
rand alsallaq <randalsallaq97@gmail.com>
2020-08-27 00:41:56 +0300

    style

---
+++ read.js
@@ -0,0 +1,7 @@
+function promptMe(){
+    var name = prompt("Please entre your name");
+    var phone = prompt("Please entre your  name");
+  document.getElementById("es").innerHTML="your order has been confirmed";
+  doc.write
+}
+
