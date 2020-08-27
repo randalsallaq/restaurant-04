2b6c304f1d9807592502aa01b5f69201b4b23e44
rand alsallaq <randalsallaq97@gmail.com>
2020-08-27 17:02:52 +0300

    while loop & validation name

--- read.js
+++ read.js
@@ -1,18 +1,40 @@
 
 
+var owner;
 // function promptMe(){
 
+    // Add validation 
     var name = prompt("Please insert your name");
+
+        while(!isNaN(name)){
+            alert('Please Enter a Name Not a Number');
+            name = prompt("Please insert your name");
+
+        }
 
     var address = prompt("Please insert your address");
 
     var phone = prompt("Please insert your phone");
-    var age = prompt("guess my age?");
-if (age<23){
+ 
+    alert('Guess the websites owner age');
+    for(var i=0;i<3;i++){
+
+
+        owner=prompt('Enter The age ');
+
+        if (owner==23){
+
+            break;
+        }
+
+    }
+
+
+if (owner<23){
     alert('low ');
-}else if( age >23){
+}else if( owner >23){
     alert('high');
-}else if(age =23)
+}else if(owner =23)
 {
     alert('i am 23');
 }else {
@@ -24,3 +46,4 @@ if (age<23){
 // promptMe();
 ///////// lab6//////
 
+((2 * 90) === 180) && (false && 'true')
