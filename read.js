69a9333cf7db22cdbc67d73277faf32404d07c38
rand alsallaq <randalsallaq97@gmail.com>
2020-08-27 03:40:21 +0300

    lab6

--- read.js
+++ read.js
@@ -1,7 +1,23 @@
+
+
 function promptMe(){
-    var name = prompt("Please entre your name");
-    var phone = prompt("Please entre your  name");
-  document.getElementById("es").innerHTML="your order has been confirmed";
-  doc.write
+    var name = prompt("Please insert your name");
+    var address = prompt("Please insert your address");
+    var phone = prompt("Please insert your phone");
+    var age = prompt("guess my age?");
+if (age<23){
+    alert('low ');
+}else if( age >23){
+    alert('high');
+}else if(age =23)
+{
+    alert('i am 23');
+}else {
+    hhhhh
 }
+  document.getElementById("es").innerHTML="your order has been confirmed";
+  
+}
+promptMe();
+///////// lab6//////
 
